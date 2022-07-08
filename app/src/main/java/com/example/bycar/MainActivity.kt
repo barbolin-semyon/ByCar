37ec717c7af861bb0d60f73fbbf0e1c7f43a7f03
syoma <skat.apps@gmail.com>
2022-07-08 23:03:26 +0300

    realization MainActivity.kt

--- app/src/main/java/com/example/bycar/MainActivity.kt
+++ app/src/main/java/com/example/bycar/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.bycar.ui.navigation.CarBottomNavigation
+import com.example.bycar.ui.navigation.CarNavHost
 import com.example.bycar.ui.theme.ByCarTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ByCarTheme {
-
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = { CarBottomNavigation(navController = navController) }
+                ) {
+                    Surface(modifier = Modifier.padding(it)) {
+                        CarNavHost(navController = navController)
+                    }
+                }
             }
         }
     }
