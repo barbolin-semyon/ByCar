39dd4ed2ccbd544c178dde40740e9ff8fc5dde9a
syoma <skat.apps@gmail.com>
2022-07-08 23:21:02 +0300

    BottomNavigation:  * add line between items menu

--- app/src/main/java/com/example/bycar/ui/navigation/CarBottomNavigation.kt
+++ app/src/main/java/com/example/bycar/ui/navigation/CarBottomNavigation.kt
@@ -1,17 +1,20 @@
 package com.example.bycar.ui.navigation
 
 import android.graphics.drawable.Icon
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.bycar.R
+import com.example.bycar.ui.theme.Gray200
 
 @Composable
 fun CarBottomNavigation(navController: NavHostController) {
@@ -26,27 +29,39 @@ fun CarBottomNavigation(navController: NavHostController) {
             BottomNavigation(backgroundColor = Color.White) {
                 screens.forEach { currentScreen ->
 
-                    val isSelected = currentDestination.hierarchy.any { it.route == currentScreen.route }
+                    val isSelected =
+                        currentDestination.hierarchy.any { it.route == currentScreen.route }
 
                     BottomNavigationItem(
                         selected = isSelected,
-                        onClick = { navController.navigate(currentScreen.route) {
-                            popUpTo(navController.graph.startDestinationId) {
-                                inclusive = true
-                                saveState = true
+                        onClick = {
+                            navController.navigate(currentScreen.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    inclusive = true
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
                             }
-                            launchSingleTop = true
-                            restoreState = true
-                        } },
-                        icon = { Icon(
-                            painter = painterResource(id = currentScreen.idImage),
-                            contentDescription = currentScreen.description
-                        ) },
+                        },
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = currentScreen.idImage),
+                                contentDescription = currentScreen.description
+                            )
+                        },
                         selectedContentColor = Color.Black,
                         unselectedContentColor = Color.Gray,
-                        label = {Text("●")},
+                        label = { Text("●") },
                         alwaysShowLabel = isSelected
                     )
+
+                    if (currentScreen.route != MainScreens.Chat.route) {
+                        Divider(modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(top = 16.dp, bottom = 16.dp)
+                            .width(1.dp), color = Gray200)
+                    }
                 }
             }
         }
